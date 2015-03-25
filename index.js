74a76b420cd3c3b15be760e6fcbdace3e484b4e3
Christian Benincasa <chrisbenincasa@gmail.com>
2015-03-24 22:42:50 -0400

    initial

--- index.js
+++ index.js
@@ -9,20 +9,24 @@ module.exports = function(options) {
     var dep = (options||{}).dependency || new LessDependency(options||{});
 
     var stream = through.obj(function(file, enc, callback) {
+        var self = this;
+
         if (file.isNull()) {
             this.push(file);
             return callback();
         }
 
-        dep.parseImports(file);
-        _.uniq(dep.accumulateDependentsOn(file.path)).forEach(function(fname) {
-            var f = file.clone();
-            f.path = fname;
-            f.contents = file.isStream() ?
-                    fs.createReadStream(fname) : fs.readFileSync(fname);
-            this.push(f);
-        }.bind(this));
-        return callback();
+        dep.parseImports(file).then(function() {
+            _.uniq(dep.accumulateDependentsOn(file.path)).forEach(function(fname) {
+                var f = file.clone();
+                f.path = fname;
+                f.contents = file.isStream() ?
+                        fs.createReadStream(fname) : fs.readFileSync(fname);
+                this.push(f);
+            }.bind(self));
+
+            callback();
+        });
     });
     return stream;
 };
